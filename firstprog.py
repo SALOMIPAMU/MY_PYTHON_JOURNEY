91d117f851ad28cd368131e549771978a4814907
SALOMIPAMU <160341954+SALOMIPAMU@users.noreply.github.com>
2025-08-17 20:13:39 +0530

    Add files via upload

---
+++ firstprog.py
@@ -0,0 +1,13 @@
+#ADDITION OF TWO NUMBERS AND HELLO WORLD
+a="hello world"
+print(a)
+b=10
+c=20
+A=b+c
+print(A)
+
+#TYPE CONVERSION FROM INT TO FLOAT& vice versa
+num_int = int(5.8)
+num_float= float(25)
+print(num_int)
+print(num_float)
